e4ad6310305477ba93de1f0e041e8ca12c208400
Rob Pinciuc <rob@pinciuc.com>
2021-11-15 23:03:30 -0800

    Add classes for active and active path menu items

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import MenuType from "@/types/menu"
+import classNames from "classnames"
 
 import utilStyles from "@/styles/utils.module.scss"
 import menuStyles from "@/styles/menu.module.scss"
@@ -10,6 +12,7 @@ type Props = {
 }
 
 export const Menu = ({ menuData }: Props) => {
+  const { asPath } = useRouter()
   const menuId = `${menuData.slug}-menu`
   return (
     <nav className={menuStyles.nav} role="navigation" aria-labelledby={menuId}>
@@ -17,11 +20,21 @@ export const Menu = ({ menuData }: Props) => {
         {menuData.title}
       </h2>
       <ul className={menuStyles.menuMain}>
-        {menuData.menu_items.map(menuItem => (
-          <li key={menuItem.id} className={menuStyles.menuItem}>
-            <Link href={menuItem.link}>{menuItem.title}</Link>
-          </li>
-        ))}
+        {menuData.menu_items.map(menuItem => {
+          const isActive = menuItem.link === asPath
+          // Active Path must match a directory, with a trailing slash, so
+          // ensure the link has a trailing slash before matching.
+          const isActivePath = asPath.startsWith(menuItem.link.replace(/\/$/, '') + '/')
+          return (
+            <li key={menuItem.id} className={menuStyles.menuItem}>
+              <Link href={menuItem.link}>
+                <a className={classNames(menuStyles.menuLink, { [menuStyles.isActive]: isActive, [menuStyles.isActivePath]: isActivePath })}>
+                  {menuItem.title}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
