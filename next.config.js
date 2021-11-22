239111ff59ffad806feb272ed937dfa9cf095c1c
Rob Pinciuc <rob@pinciuc.com>
2021-11-21 16:22:42 -0800

    Add S3 image source to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp", "image/jpeg"],
-    domains: ["localhost", "strapi.umami.pinciuc.com"],
+    domains: [
+      "localhost",
+      "strapi.umami.pinciuc.com",
+      "umami-strapi-uploads.s3.ca-central-1.amazonaws.com",
+    ],
   },
 }
